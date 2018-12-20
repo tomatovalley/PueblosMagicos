d226b90943c7998aec97d547bf13c507bff28e29
JuanDelgado2 <juan.delgado16@tectijuana.edu.mx>
2018-12-20 11:01:54 -0800

    passport terminado

--- config/passport.js
+++ config/passport.js
@@ -24,16 +24,56 @@ module.exports = function (passport){
 		pasaReqToCallback: true
 	},
 
+	
 	function (req, name, username, email, password, done) {
-		user.findOne({$or: [{'local.username': username}, {'local.email': email}]}, function(err, user) {
+		user.findOne({'local.username': username}, function(err, user) {
 			if ( err ){
 				return done( err );
 			}
 			if ( user ) {
-				return done( null, false, req.flash('signupMessage', ''))
+				return done( null, false, req.flash('signupMessage', 'El nombre de usuario ya existe'));
+
+			} else {
+				var newUser = new User();
+				newUser.local.name = name;
+				newUser.local.username = username;
+				newUser.local.email = email;
+				newUser.local.password = newUser.generateHash(password);
+				newUser.save(function (err) {
+					if (err) { 
+						throw err; 
+					}
+					return done( null, newUser );
+				});
+
 			}
 		})
-	}
-	))
+	}));
+
+
+	passport.user( 'local-login', new passportlocal ({
+		usernameField: 'username',
+		passwordField: 'password',
+		pasaReqToCallback: true
+	},
+
+	function (req, username, password, done) {
+		user.findOne({'local.username': username}, function(err, user) {
+			if ( err ){
+				return done( err );
+			}
+			if ( user ) {
+				return done( null, false, req.flash('loginMessage', 'Usuario no encontrado'));
+
+			} 
+			if ( !user.validatePassword( password ) ) {
+			
+				return done( null, false, req.flash('loginMessage', 'Contraseña no valida'));				
+					
+				}
+			return done( null, newUser );
+			
+		} )
+	}));
 
 }
