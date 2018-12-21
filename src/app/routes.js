a9c8a2601675640fae69fc0ad91cbe2a2d9fe271
Jorge <jorge.rosal16@tectijuana.edu.mx>
2018-12-21 11:00:03 -0800

    views y css

---
+++ src/app/routes.js
@@ -0,0 +1,18 @@
+module.exports = (app, passport) => {
+
+  app.get('/', (req, res) => {
+    res.render('index')
+  });
+
+  app.get('/login', (req, res) => {
+    res.render('login', {
+      message: req.flash('loginMessage')
+    })
+  });
+
+  app.get('/signup', (req, res) => {
+    res.render('signup', {
+      message: req.flash('signupMessage')
+    })
+  });
+};
